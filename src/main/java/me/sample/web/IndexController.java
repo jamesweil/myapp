7a2cfdd21278f3d64d1eb056e7cb7b2e36682cc4
jameswei <jamesweil2015@gmail.com>
2019-08-12 00:05:31 +0800

    add springboot

--- src/main/java/me/sample/web/IndexController.java
+++ src/main/java/me/sample/web/IndexController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class IndexController {
 
-    @RequestMapping("/")
+    @RequestMapping("/index")
     @ResponseBody
     public String index() {
         return "Hello Springboot.";
